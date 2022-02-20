a1f9b2d00b6e456b9150b2d719e7d369b383cee1
huseyin saglam <huseyinnsaglamm@gmail.com>
2022-02-20 18:18:10 +0300

    used moment timezone, update time by selected counrty.

--- js/showTime.js
+++ js/showTime.js
@@ -1,35 +1,47 @@
-function currentTime() {
-  let date = new Date();
-  let days=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
-  let year=date.getFullYear();
-  let month=date.getMonth()+1;
-  let day=date.getDate();
-  let dayWeek=date.getDay();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  let session = "AM";
-  if(hours === 0){
-    hours = 12;
-  }
-  if(hours > 12){
-    hours = hours - 12;
-    session = "PM";
-  }
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
+import moment from "moment-timezone";
 
-  //todo ay patladı ezgiye sorulacak
-  let dateDay=days[date.getDay()]+" ";
-  let dateTime= " "+" "+day +"/"+month+"/"+year;
-  let times = hours + ":" + minutes + ":" + seconds ;
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+let timeZone = "Europe/Istanbul";
 
-  document.getElementById("day").innerText=dateDay;
-  document.getElementById("date").innerText=dateTime;
-  document.getElementById("meridian").innerText=session;
-  document.getElementById("clock").innerText = times;
-  let t = setTimeout(function(){ currentTime() }, 1000);
+document.getElementById("mySelect").addEventListener("change", selectedCountryOptionChange);
+
+function selectedCountryOptionChange() {
+  const select = document.getElementById("mySelect");
+  timeZone = select.value;
+  currentTime();
 }
-currentTime();
 
+function currentTime() {
+  document.getElementById("day").innerText = days[getCurrentTime(timeZone).days()];
+  document.getElementById("date").innerText = getCurrentDateString(timeZone);
+  document.getElementById("meridian").innerText = getMeridian(getCurrentTime(timeZone).hour());
+  document.getElementById("clock").innerText = getCurrentHourString(timeZone);
+  let timer = setTimeout(function () {
+    currentTime()
+  }, 1000);
+}
+
+function getCurrentDateString(timeZoneCode) {
+  return moment(new Date()).tz(timeZoneCode).format('DD/MM/YYYY');
+}
+
+function getCurrentTime(timeZoneCode) {
+  return moment(new Date()).tz(timeZoneCode);
+}
+
+function getCurrentHourString(timeZoneCode) {
+  return moment(new Date()).tz(timeZoneCode).format('HH:mm:ss');
+}
+
+function getMeridian(hour) {
+  let meridian = "AM";
+  if (hour === 0) {
+    hour = 12;
+  }
+  if (hour > 12) {
+    meridian = "PM";
+  }
+  return meridian;
+}
+
+currentTime();
